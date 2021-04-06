ad7e11d4f11536b54c30757019f0307fff082e61
Jun Wen <e0421529@u.nus.edu>
2021-04-06 08:20:04 +0800

    worked on functionality of marketplace->indv item

--- src/components/Item.vue
+++ src/components/Item.vue
@@ -9,7 +9,10 @@ import firebase from "firebase/app";
 export default {
     mounted() {
         this.setupFirebase();
+        this.fetchItem();
+        console.log(this.token);
     },
+
     methods:{
         setupFirebase() {
             firebase.auth().onAuthStateChanged(user => {
@@ -23,13 +26,25 @@ export default {
                     this.currentUser = false;
                 }
             });
+        },
+        fetchItem() {
+            var db = firebase.firestore();
+            db.collection('marketplace').where('imageRef', '==', this.id).get()
+            .then((querySnapshot) => {
+                querySnapshot.forEach((doc) => {
+                    console.log(doc.id);
+                    this.item.push(doc.data());
+                    console.log(this.item);
+                });
+            });
         }
     },
     data(){
         return {
             loggedIn: false,
             currentUser: false,
-            id: this.$route.query.id,
+            id: this.$route.query.id + '&token=' + this.$route.query.token,
+            item: []
         }
     }
 }
