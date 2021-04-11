cded9b1d3d6789f02382b4134f3b514c991f49b8
jclee98 <junchenlee98@gmail.com>
2021-04-12 01:59:32 +0800

    Added notification for profile

--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -2,15 +2,14 @@
     <div class="profile-container">
         <div class="profile-info">
             <div class="profile-name">
-                <!--<img src="https://i.postimg.cc/yNMnZJp9/blank-profile-picture-973460-1280-1.png">-->
                 <img :src='this.url' id="profile-img"><br>
                 <div id="displayName">{{this.currentUser.displayName}}</div>
                 <br><br>
                 <router-link to="/editprofile" tag="button-editprofile" exact>Edit Profile</router-link>
             </div>
             <div class="bells-and-chat">
-                <img id="bell-img" src="https://i.postimg.cc/259ychNb/Bell.png">
-                <img id="bell-img" src="https://i.postimg.cc/HWynzrZn/No-Bell.png">
+                <img id="bell-img" src="https://i.postimg.cc/259ychNb/Bell.png" onclick="document.getElementById('bell-img').src = 'https://i.postimg.cc/d1s5KtqC/Bell-blue.png'; document.getElementById('nobell-img').src = 'https://i.postimg.cc/HWynzrZn/No-Bell.png'; alert('Notifications Turned On')">
+                <img id="nobell-img" src="https://i.postimg.cc/HWynzrZn/No-Bell.png" onclick="document.getElementById('nobell-img').src = 'https://i.postimg.cc/C5qgg1K7/No-Bell-blue.png'; document.getElementById('bell-img').src = 'https://i.postimg.cc/259ychNb/Bell.png'; alert('Notifications Turned Off')">
                 <img id="chat-img" src="https://i.postimg.cc/kGbcQnmX/Chat-Icon.png" @click="goToChatList">
             </div>
             <div class="num-items">
