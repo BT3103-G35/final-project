9585e99dd39cbf6aa8104fae8e00a28171fe49c7
Jun Wen <e0421529@u.nus.edu>
2021-04-13 06:18:22 +0800

    fixed dashboard showing marketplace clothing data

--- src/components/BarChart.js
+++ src/components/BarChart.js
@@ -4,10 +4,15 @@ import firebase from "firebase/app";
 
 
 export default{
-
     extends: Bar,
+    created(){
+        this.setupFirebase();
+        this.fetchItems()
+    },
     data: function() {
         return {
+            loggedIn: false,
+            currentUser: false,
             results: [0, 0, 0, 0, 0, 0, 0, 0],
             datacollection: {
                 labels: ['top', 'bottom', 'outerwear', 'footwear', 'headwear', 'jewellery', 'accessory', 'others'],
@@ -50,8 +55,8 @@ export default{
 
         fetchItems: function(){
             var db = firebase.firestore();
-            db.collection('marketplace').get().then((SnapShot) =>{
-                SnapShot.forEach((doc) => {
+            db.collection('marketplace').where('user', '==', this.currentUser.uid).get().then((querySnapShot) =>{
+                querySnapShot.forEach((doc) => {
                     if (doc.data().category == 'top'){
                         this.results[0] += 1;
                     }else if (doc.data().category == 'bottom'){
@@ -75,10 +80,21 @@ export default{
                 this.renderChart(this.datacollection, this.options)
             })
 
-        }
-    },
-    created(){
-        this.fetchItems()
+        },
+        setupFirebase() {
+            firebase.auth().onAuthStateChanged(user => {
+                if (user) {
+                    // User is signed in.
+                    this.loggedIn = true;
+                    this.currentUser = firebase.auth().currentUser;
+                    this.fetchItems();
+                } else {
+                    // No user is signed in.
+                    this.loggedIn = false;
+                    this.currentUser = false;
+                }
+            });
+        },
     }
 }
 
